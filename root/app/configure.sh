bad34954a90df6e5d85293090b5880a867e92135
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-12-13 13:49:23 -0800

    Fixes

--- root/app/configure.sh
+++ root/app/configure.sh
@@ -4,13 +4,10 @@ if [ "${PACKAGES}" != "" ]; then
   (cd /app ; npm --save install ${PACKAGES})
 fi
 
-ACCESSORIES=$(cat /app/accessories-config.json)
-PLATFORMS=$(cat /app/platforms-config.json)
-
 cat /app/config.json | sed \
   -e "s/{{HOSTNAME}}/${HOSTNAME}/g" \
   -e "s/{{BRIDGE_USERNAME}}/${BRIDGE_USERNAME}/g" \
   -e "s/{{BRIDGE_PIN}}/${BRIDGE_PIN}/g" \
-  -e "s/{{ACCESSORIES}}/${ACCESSORIES}/g" \
-  -e "s/{{PLATFORMS}}/${PLATFORMS}/g" \
+  -e "/{{ACCESSORIES}}/r /app/accessories-config.json" -e "s/{{ACCESSORIES}}//" \
+  -e "/{{PLATFORMS}}/r /app/platforms-config.json" -e "s/{{PLATFORMS}}//" \
   > /app/homebridge/config.json
