d2fa7dc7e0507b07f2cf4238629f9c33bb131606
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-05-25 16:59:11 -0700

    qrcodes

--- root/app/configure.sh
+++ root/app/configure.sh
@@ -8,6 +8,7 @@ cat /app/config.json | sed \
   -e "s/{{HOSTNAME}}/${HOSTNAME}/g" \
   -e "s/{{BRIDGE_USERNAME}}/${BRIDGE_USERNAME}/g" \
   -e "s/{{BRIDGE_PIN}}/${BRIDGE_PIN}/g" \
+  -e "s/{{BRIDGE_SETUPID}}/${BRIDGE_SETUPID}/g" \
   -e "/{{ACCESSORIES}}/r /app/accessories-config.json" -e "s/{{ACCESSORIES}}//" \
   -e "/{{PLATFORMS}}/r /app/platforms-config.json" -e "s/{{PLATFORMS}}//" \
   > /app/homebridge/config.json
